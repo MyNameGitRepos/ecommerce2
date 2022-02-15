10c0a305688bbca33c212078de8b87e376cc5b50
mynamegitrepos <samuelpereiradesouza665@gmail.com>
2022-02-15 18:03:41 -0300

    login screen

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page{
 private $tpl;
 private $options =[];
 private $defaults = [
+	"header"=>true,
+	"footer"=>true,
   "data"=>[]
 ];
 
@@ -27,7 +29,7 @@ public function __construct($opts = array(), $tpl_dir = "/views/"){
 	Tpl::configure( $config );
 	$this->tpl = new Tpl;
     $this->setData($this->options["data"]);
-	$this->tpl->draw("header");
+	if($this->options["header"] === true) $this->tpl->draw("header");
 }
 
 public function setData($data = array()){
@@ -42,7 +44,7 @@ public function setTpl($name, $data = array(), $returnHTML = false){
 }
 
 public function __destruct(){
-    $this->tpl->draw("footer");
+    if($this->options["footer"] === true) $this->tpl->draw("footer");
 }
 
 }
