9f92af2d22dc5133dcbf59b9c6ec317bf153910b
mynamegitrepos <samuelpereiradesouza665@gmail.com>
2022-03-10 15:48:46 -0300

    Alterar senha

--- admin-users.php
+++ admin-users.php
@@ -3,8 +3,46 @@
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 
-$app->get('/admin/users', function(){
-    
+$app->get("/admin/users/:iduser/password", function($iduser){
+   User::verifyLogin();
+   $user = new User();
+   $user->get((int)$iduser);
+   $page = new PageAdmin();
+   $page->setTpl("users-password", [
+     "user"=>$user->getValues(),
+     "msgError"=>User::getError(),
+     "msgSuccess"=>User::getSuccess()
+   ]);
+});
+
+$app->post("/admin/users/:iduser/password", function($iduser){
+   User::verifyLogin();
+   if(!isset($_POST['despassword']) || $_POST['despassword'] === ''){
+    User::setError("Preencha a nova sennha!");
+    header("Location: /admin/users/$iduser/password");
+    exit;
+   }
+   if(!isset($_POST['despassword-confirm']) || $_POST['despassword-confirm'] === ''){
+    User::setError("Preencha a confirmação da nova sennha!");
+    header("Location: /admin/users/$iduser/password");
+    exit;
+   }
+   if($_POST['despassword'] !== $_POST['despassword-confirm']){
+    User::setError("Preencha corretamente as sennha!");
+    header("Location: /admin/users/$iduser/password");
+    exit;
+   }
+
+   $user = new User();
+   $user->get((int)$iduser); 
+   $user->setPassword(User::getPasswordHash($_POST['despassword']));
+   $user->setSuccess("Senha alterada com sucesso!");
+   header("Location: /admin/users/$iduser/password");
+    exit;
+   
+});
+
+$app->get('/admin/users', function(){    
     User::verifyLogin();
     $search = (isset($_GET['search'])) ? $_GET['search'] : "";
     $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
@@ -15,7 +53,6 @@ $app->get('/admin/users', function(){
     } 
     $pages = [];
     for($x = 0; $x < $pagination['pages']; $x++){
-
       array_push($pages, [ 
        'href'=>'/admin/users?'.http_build_query([
           'page'=>$x+1,
